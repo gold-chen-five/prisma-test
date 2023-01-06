e5755d2b98ce0709fa3e3c4879ba457775456fb4
phalanity <phalanity2@gmail.com>
2023-01-06 17:09:06 +0800

    next.config header add

--- next.config.js
+++ next.config.js
@@ -1,7 +1,57 @@
 /** @type {import('next').NextConfig} */
+const ContentSecurityPolicy = `
+  default-src 'self';
+  script-src 'self' 'unsafe-eval' 'nonce-cm1vaw==';
+  style-src 'self' 'unsafe-inline';
+  font-src 'self';
+  img-src 'self';
+  connect-src 'self';
+  frame-src 'self';
+  frame-ancestors 'self';
+  form-action 'self'
+
+`
+const securityHeaders = [
+  {
+    key: 'Content-Security-Policy',
+    value: ContentSecurityPolicy.replace(/\s{2,}/g, ' ').trim()
+  },
+  {
+    key: 'X-Frame-Options',
+    value: 'SAMEORIGIN'
+  },
+  {
+    key: 'X-Content-Type-Options',
+    value: 'nosniff'
+  },
+  {
+    key: 'X-XSS-Protection',
+    value: '1; mode=block'
+  },
+  {
+    key: 'Strict-Transport-Security',
+    value: 'max-age=63072000; includeSubDomains; preload'
+  },
+  {
+    key: 'X-DNS-Prefetch-Control',
+    value: 'on'
+  }
+]
+
 const nextConfig = {
+  poweredByHeader: false,
   reactStrictMode: true,
   swcMinify: true,
+  async headers(){
+    return [
+      {
+        // Apply these headers to all routes in your application.
+        source: '/:path*',
+        headers: securityHeaders,
+      },
+    ]
+    
+  }
 }
 
 module.exports = nextConfig
